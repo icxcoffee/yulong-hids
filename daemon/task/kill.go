26e77debf1fb54623cf72de0e7beaa16d346cfde
icxcoffee <513321675@qq.com>
2018-11-06 16:53:31 +0800

    Update kill.go

--- daemon/task/kill.go
+++ daemon/task/kill.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"regexp"
 	"runtime"
+	"strings"
 	"yulong-hids/daemon/common"
 )
 
 // KillProcess 根据进程名结束进程
 func KillProcess(processName string) string {
 	var data string
+	path := strings.Split(strings.Split(processName, " ")[0], "/")
+	processName = path[len(path)-1]
 	if ok, _ := regexp.MatchString(`^[a-zA-Z0-1\.\-_]+$`, processName); !ok {
 		return ""
 	}
