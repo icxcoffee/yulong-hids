0efc1d3e6d39d8e3d5d7593f596ab9617b94c85c
icxcoffee <513321675@qq.com>
2018-11-06 17:46:16 +0800

    Update kill.go

--- daemon/task/kill.go
+++ daemon/task/kill.go
@@ -2,24 +2,21 @@ package task
 
 import (
 	"fmt"
-	"regexp"
 	"runtime"
 	"strings"
 	"yulong-hids/daemon/common"
 )
 
 // KillProcess 根据进程名结束进程
-func KillProcess(processName string) string {
+func KillProcess(processName string) (string, error) {
 	var data string
+	var err error
 	path := strings.Split(strings.Split(processName, " ")[0], "/")
 	processName = path[len(path)-1]
-	if ok, _ := regexp.MatchString(`^[a-zA-Z0-1\.\-_]+$`, processName); !ok {
-		return ""
-	}
 	if runtime.GOOS == "windows" {
-		data, _ = common.CmdExec("taskkill.exe /f /im " + processName)
+		data, err = common.CmdExec("taskkill.exe /f /im " + processName)
 	} else {
-		data, _ = common.CmdExec(fmt.Sprintf("kill -9 $(pidof %s)", processName))
+		data, err = common.CmdExec(fmt.Sprintf("kill -9 $(pidof %s)", processName))
 	}
-	return data
+	return data, err
 }
